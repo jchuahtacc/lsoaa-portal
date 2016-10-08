7ca994739f4f0753c9794ef0da4dbf4deab16423
marinaesca <marinaescalante@utexas.edu>
2016-10-08 13:30:27 -0500

    :( still the mentors and coaches

--- public/js/coach.js
+++ public/js/coach.js
@@ -27,7 +27,7 @@ function ReadCoachInfo(userId, callback)
 
 coachData = {"name": "Bob", "email": "bob@bob.com", "cellNumber": "1234567890", "schoolName": "FallField Elementary", "address": "1234 school lane"};
 
-makeCoach(coachData);
+//makeCoach(coachData);
 getCoachInfo("23456",function(data) {
 	console.log("coach info", data);
 });
