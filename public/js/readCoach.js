cb460a206856b8f6a77250fef5f8a3b04f5ac6c9
marinaesca <marinaescalante@utexas.edu>
2016-10-08 13:17:03 -0500

    made read/ write files

--- public/js/readCoach.js
+++ public/js/readCoach.js
@@ -1,6 +1,5 @@
-console.log("Hello world");
 
-function makeCoach(coachData)
+function writeCoach(coachData)
 {
 	console.log(coachData);
 	var userId = "23456";
@@ -10,7 +9,7 @@ function makeCoach(coachData)
 	
 }
 
-function getCoachInfo(userId, callback)
+function ReadCoachInfo(userId, callback)
 {
 	var reference = firebase.database().ref('coaches').child(userId);
 	reference.once('value')
@@ -24,8 +23,6 @@ function getCoachInfo(userId, callback)
         }
     );
 }
-
-//take up coach info, store, where am i storing
 
 
 coachData = {"name": "Bob", "email": "bob@bob.com", "cellNumber": "1234567890", "schoolName": "FallField Elementary", "address": "1234 school lane"};
